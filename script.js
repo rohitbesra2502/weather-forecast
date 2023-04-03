118fb012a26ac1ead843412b5371873a84039337
Rohit Besra <rohitkumarbesra786@gmail.com>
2023-04-03 22:14:24 +0530

    weather-website-fix-main-screen

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ const  apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=
 const searchBox = document.querySelector(".input");
 const searchBtn = document.querySelector(".btn");
 const weatherIcon = document.querySelector(".weather-icon");
-const bgImage = document.querySelector(".weather-img");
+const bgImage = document.querySelector(".bg-img");
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     if(response.status == 404){
-            document.querySelector(".error").style.display = "block";
-            document.querySelector(".weather-details").style.display = "none";
+        document.querySelector(".error").style.display = "block";
     }else{
         var data = await response.json();
         document.querySelector(".city").innerHTML = data.name;
@@ -18,19 +17,19 @@ async function checkWeather(city){
 
         if(data.weather[0].main == "Clouds"){
             weatherIcon.src = "images/clouds.png";
-            bgImage.src="images/bg-img/cloud.jpg";
+            bgImage.style.backgroundImage = "url('images/bg-img/cloud.jpg')";
         }else if(data.weather[0].main == "Clear"){
             weatherIcon.src = "images/clear.png";
-            bgImage.src="images/bg-img/clear.jpg";
+            bgImage.style.backgroundImage = "url('images/bg-img/clear.jpg')";
         }else if(data.weather[0].main == "Rain"){
             weatherIcon.src = "images/rain.png";
-            bgImage.src="images/bg-img/rain.jpg";
+            bgImage.style.backgroundImage = "url('images/bg-img/rain.jpg')";
         }else if(data.weather[0].main == "Drizzle"){
             weatherIcon.src = "images/drizzle.png";
-            bgImage.src="images/bg-img/drizzle.jpg";
+            bgImage.style.backgroundImage = "url('images/bg-img/drizzle.jpg')";
         }else if(data.weather[0].main == "Mist"){
             weatherIcon.src = "images/mist.png";
-            bgImage.src="images/bg-img/mist.jpg";
+            bgImage.style.backgroundImage = "url('images/bg-img/mist.jpg')";
         }
         // document.querySelector(".weather-details").style.display = "block";
         // document.querySelector(".error").style.display = "none";
@@ -50,10 +49,10 @@ function GetInfo(city){
     .then(response => response.json())
     .then(data => {
         for(i = 0; i < 7; i++){
-            document.getElementById("day"+(i+1)+"Min").innerHTML = "Min:" +Number(data.list[i].main.temp_min-273.15).toFixed(1)+"°";
+            document.getElementById("day"+(i+1)+"Min").innerHTML = "Min:" +Math.round(Number(data.list[i].main.temp_min-273.15).toFixed(1))+"&#8451";
         }
         for(i = 0; i < 7; i++){
-            document.getElementById("day"+(i+1)+"Max").innerHTML = "Max:" +Number(data.list[i].main.temp_max-273.15).toFixed(1)+"°";
+            document.getElementById("day"+(i+1)+"Max").innerHTML = "Max:" +Math.round(Number(data.list[i].main.temp_max-273.15).toFixed(1))+"&#8451";
         }
         for(i = 0; i < 7; i++){
             document.getElementById("img"+(i+1)).src = "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon+".png";
